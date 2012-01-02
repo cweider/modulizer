87970f5de2c898a2171c3e9e721935f83e23fd65
Chad Weider <cweider@oofn.net>
2012-01-02 13:16:29 -0800

    MockRequire uses URIs and supports local and remote resources.

--- mock_require.js
+++ mock_require.js
@@ -1,14 +1,162 @@
 var fs = require('fs');
 var pathutil = require('path');
+var urlutil = require('url');
 var events = require('events');
 
 var kernelPath = pathutil.join(__dirname, 'kernel.js');
-var kernel = '(function (XMLHttpRequest) {var require = '
-  + fs.readFileSync(kernelPath, 'utf8')
-  + ';\nreturn require;})';
+var kernel = fs.readFileSync(kernelPath, 'utf8');
 
-var buildKernel = require('vm').runInThisContext(kernel, kernelPath);
-var buildMockXMLHttpRequestClass = function (virtualPaths) {
+var buildKernel = require('vm').runInThisContext(
+  '(function (XMLHttpRequest) {var require = '
+  + kernel + ';\n'
+  + 'return require;})', kernelPath);
+
+/* Cheap URL request implementation */
+var fs_client = (new function () {
+  var STATUS_MESSAGES = {
+    403: '403: Access denied.'
+  , 404: '404: File not found.'
+  , 405: '405: Only the HEAD or GET methods are allowed.'
+  , 500: '500: Error reading file.'
+  };
+
+  function request(options, callback) {
+    var path = options.path;
+    var method = options.method;
+
+    var response = new (require('events').EventEmitter);
+    response.setEncoding = function (encoding) {this._encoding = encoding};
+    response.statusCode = 504;
+    response.headers = {};
+
+    var request = new (require('events').EventEmitter);
+    request.end = function () {
+      if (options.method != 'HEAD' && options.method != 'GET') {
+        response.statusCode = 405;
+        response.headers['Allow'] = 'HEAD, GET';
+
+        callback(response);
+        response.emit('data', STATUS_MESSAGES[response.statusCode])
+        response.emit('end');
+      } else {
+        fs.stat(path, function (error, stats) {
+          if (error) {
+            if (error.code == 'ENOENT') {
+              response.StatusCode = 404;
+            } else if (error.code == 'EACCESS') {
+              response.StatusCode = 403;
+            } else {
+              response.StatusCode = 502;
+            }
+          } else if (stats.isFile()) {
+            var date = new Date()
+            var modifiedLast = new Date(stats.mtime);
+            var modifiedSince = (options.headers || {})['if-modified-since'];
+
+            response.headers['Date'] = date.toUTCString();
+            response.headers['Last-Modified'] = modifiedLast.toUTCString();
+
+            if (modifiedSince && modifiedLast
+                && modifiedSince >= modifiedLast) {
+              response.StatusCode = 304;
+            } else {
+              response.statusCode = 200;
+            }
+          } else {
+            response.StatusCode = 404;
+          }
+
+          if (method == 'HEAD') {
+            callback(response);
+            response.emit('end');
+          } else if (response.statusCode != 200) {
+            response.headers['Content-Type'] = 'text/plain; charset=utf-8';
+
+            callback(response);
+            response.emit('data', STATUS_MESSAGES[response.statusCode])
+            response.emit('end');
+          } else {
+            fs.readFile(path, function (error, text) {
+              if (error) {
+                if (error.code == 'ENOENT') {
+                  response.statusCode = 404;
+                } else if (error.code == 'EACCESS') {
+                  response.statusCode = 403;
+                } else {
+                  response.statusCode = 502;
+                }
+                response.headers['Content-Type'] = 'text/plain; charset=utf-8';
+
+                callback(response);
+                response.emit('data', STATUS_MESSAGES[response.statusCode])
+                response.emit('end');
+              } else {
+                response.statusCode = 200;
+                response.headers['Content-Type'] =
+                    'application/javascript; charset=utf-8';
+
+                callback(response);
+                response.emit('data', text);
+                response.emit('end');
+              }
+            });
+          }
+        });
+      }
+    };
+    return request;
+  }
+  this.request = request;
+}());
+
+function requestURL(url, method, headers, callback) {
+  var parsedURL = urlutil.parse(url);
+  var client = undefined;
+  if (parsedURL.protocol == 'file:') {
+    client = fs_client;
+  } else if (parsedURL.protocol == 'http:') {
+    client = require('http');
+  } else if (parsedURL.protocol == 'https:') {
+    client = require('https');
+  }
+  if (client) {
+    var request = client.request({
+      host: parsedURL.host
+    , port: parsedURL.port
+    , path: parsedURL.path
+    , method: method
+    , headers: headers
+    }, function (response) {
+      var buffer = undefined;
+      response.setEncoding('utf8');
+      response.on('data', function (chunk) {
+        buffer = buffer || '';
+        buffer += chunk;
+      });
+      response.on('close', function () {
+        callback(502, {});
+      });
+      response.on('end', function () {
+        callback(response.statusCode, response.headers, buffer);
+      });
+    });
+    request.on('error', function () {
+      callback(502, {});
+    });
+    request.end();
+  }
+}
+
+function normalizePathAsURI(path) {
+  var parsedUrl = urlutil.parse(path);
+  if (parsedUrl.protocol === undefined) {
+    parsedUrl.protocol = 'file:';
+    parsedUrl.path = pathutil.resolve(parsedUrl.path);
+  }
+  return urlutil.format(parsedUrl);
+}
+
+var buildMockXMLHttpRequestClass = function () {
   var emitter = new events.EventEmitter();
   var requestCount = 0;
   var idleTimer = undefined;
@@ -33,58 +181,45 @@ var buildMockXMLHttpRequestClass = function (virtualPaths) {
   MockXMLHttpRequest.prototype = new function () {
     this.open = function(method, url, async) {
       this.async = async;
-      this.url = url;
+      this.url = normalizePathAsURI(url);
     }
+    this.withCredentials = false; // Pass CORS capability checks.
     this.send = function () {
-      var requestPath;
-      var path;
-      var basePath;
-      var realPath;
-
-      var components = this.url.split('/');
-      for (var i = 0, ii = components.length; i < ii; i++) {
-        components[i] = decodeURIComponent(components[i]);
-      }
-      requestPath = components.join('/')
-      for (var virtualPath in virtualPaths) {
-        if (Object.prototype.hasOwnProperty.call(virtualPaths, virtualPath)) {
-          var testPath = requestPath.slice(0, virtualPath.length);
-          if (testPath == virtualPath) {
-            path = requestPath.slice(virtualPath.length, requestPath.length);
-            basePath = virtualPaths[virtualPath];
-            realPath = pathutil.join(basePath, path);
-            break;
-          }
-        }
-      }
+      var parsedURL = urlutil.parse(this.url);
 
       var info = {
         async: !!this.async
-      , requestPath: requestPath
-      , path: path
-      , basePath: basePath
-      , realPath: realPath
+      , url: this.url
       };
-      requested(info);
+
       if (!this.async) {
-        try {
-          this.status = 200;
-          this.responseText = fs.readFileSync(realPath);
-        } catch (e) {
-          this.status = 404;
+        if (parsedURL.protocol == 'file:') {
+          requested(info);
+          try {
+            this.status = 200;
+            this.responseText = fs.readFileSync(parsedURL.path);
+          } catch (e) {
+            this.status = 404;
+          }
+          this.readyState = 4;
+          responded(info);
+        } else {
+          throw "The resource at " + JSON.stringify(this.url)
+            + " cannot be retrieved synchronously.";
         }
-        this.readyState = 4;
-        responded(info);
       } else {
         var self = this;
-        fs.readFile(realPath, 'utf8', function (error, text) {
-          self.status = error ? 404 : 200;
-          self.responseText = text;
-          self.readyState = 4;
-          var handler = self.onreadystatechange;
-          handler && handler();
-          responded(info);
-        });
+        requestURL(this.url, 'GET', {},
+          function (status, headers, content) {
+            self.status = status;
+            self.responseText = content;
+            self.readyState = 4;
+            var handler = self.onreadystatechange;
+            handler && handler();
+            responded(info);
+          }
+        );
+        requested(info);
       }
     }
   };
@@ -94,14 +229,16 @@ var buildMockXMLHttpRequestClass = function (virtualPaths) {
 }
 
 function requireForPaths(rootPath, libraryPath) {
-  var virtualPaths = {
-    root: rootPath
-  , library: libraryPath
-  };
-  var MockXMLHttpRequest = buildMockXMLHttpRequestClass(virtualPaths);
+  var MockXMLHttpRequest = buildMockXMLHttpRequestClass();
   var mockRequire = buildKernel(MockXMLHttpRequest);
-  mockRequire.setRootURI('root');
-  mockRequire.setLibraryURI('library');
+
+  if (typeof rootPath != undefined) {
+    mockRequire.setRootURI(normalizePathAsURI(rootPath));
+  }
+  if (typeof libraryPath != undefined) {
+    mockRequire.setLibraryURI(normalizePathAsURI(libraryPath));
+  }
+
   mockRequire.emitter = MockXMLHttpRequest.emitter;
   return mockRequire;
 }
