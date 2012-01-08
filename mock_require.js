bc8bba231a1915196ef29ff69a72aaa0a7bfe309
Chad Weider <cweider@oofn.net>
2012-01-08 03:38:52 -0800

    Update kernel.

--- mock_require.js
+++ mock_require.js
@@ -1,3 +1,27 @@
+/*!
+
+  Copyright (c) 2011 Chad Weider
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
 var fs = require('fs');
 var pathutil = require('path');
 var urlutil = require('url');
@@ -7,9 +31,7 @@ var kernelPath = pathutil.join(__dirname, 'kernel.js');
 var kernel = fs.readFileSync(kernelPath, 'utf8');
 
 var buildKernel = require('vm').runInThisContext(
-  '(function (XMLHttpRequest) {var require = '
-  + kernel + ';\n'
-  + 'return require;})', kernelPath);
+  '(function (XMLHttpRequest) {return ' + kernel + '})', kernelPath);
 
 /* Cheap URL request implementation */
 var fs_client = (new function () {
@@ -240,6 +262,13 @@ function requireForPaths(rootPath, libraryPath) {
   }
 
   mockRequire.emitter = MockXMLHttpRequest.emitter;
+
+  mockRequire._compileFunction = function (code, filename) {
+    return require('vm').runInThisContext('(function () {'
+      + code + '\n'
+      + '})', filename);
+  };
+
   return mockRequire;
 }
 
